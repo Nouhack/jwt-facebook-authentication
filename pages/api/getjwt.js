44ea1111ae20908f24d898a125bafe649bd72c88
Your Name <you@example.com>
2021-02-11 19:08:56 +0100

    get jwt payload

--- pages/api/getjwt.js
+++ pages/api/getjwt.js
@@ -3,8 +3,7 @@ import jwt from "next-auth/jwt";
 const secret = process.env.SECRET;
 
 export default async (req, res) => {
-  const token = await jwt.getToken({ req, secret, raw: true });
-  console.log(token);
+  const token = await jwt.getToken({ req, secret });
 
   res.send(JSON.stringify(token, null, 2));
 };
