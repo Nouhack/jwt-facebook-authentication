e93d804ea06c75028aeaf643e421907458f0f563
Your Name <you@example.com>
2021-02-11 13:05:15 +0100

    test if vercel is reading secret env var

--- pages/api/getjwt.js
+++ pages/api/getjwt.js
@@ -3,6 +3,10 @@ import jwt from "next-auth/jwt";
 const secret = process.env.SECRET;
 
 export default async (req, res) => {
+  console.log("==============");
+  console.log(secret);
+  console.log("==============");
+
   const token = await jwt.getToken({ req, secret });
   res.send(JSON.stringify(token, null, 2));
 };
